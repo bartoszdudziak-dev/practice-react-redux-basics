bad5d03455aa271ea98480a9e20ec381d8c426e4
Bartosz Dudziak <bartoszdudziak.dev@gmail.com>
2025-02-11 00:05:53 +0100

    Task 03

--- src/components/App.js
+++ src/components/App.js
@@ -8,10 +8,24 @@ import Task05 from './../../05/Task05';
 
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(() => {
-  return { message: "Działa!" };
-});
+const initState = {
+  message: "Działa!",
+  time: new Date(),
+};
+
+const reducer = (state = initState, action) => {
+  switch (action.type) {
+    case "getCurrentTime":
+      return { ...state, time: new Date() };
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer, composeWithDevTools());
 
 const App = () => {
   return (
@@ -19,8 +33,8 @@ const App = () => {
       <Task01 />
       <Provider store={store}>
         <Task02 />
+        <Task03 />
       </Provider>
-      {/* <Task03 /> */}
       {/* <Task04 /> */}
       {/* <Task05 /> */}
     </>
